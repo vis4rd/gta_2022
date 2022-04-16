b7259c46ede47aab550bc02de46ffcb5e26a7c9f
vis4rd <alagaesia.vrael@gmail.com>
2022-04-16 15:54:18 +0200

    naprawiono rysowanie grafow

--- Functions/drawG.py
+++ Functions/drawG.py
@@ -3,7 +3,20 @@ import matplotlib.pyplot as plt
 
 def DrawGraph(matrix):
 
-    G = nx.DiGraph()
-    for r in matrix:
-        for c in r:
-            G.add_edges_from([r+1,c+1])
+    graph = nx.DiGraph()
+
+    node_neighbors = [ ]
+    current_node = 1
+    for neighbors in matrix:
+        for neighbor in neighbors:
+            node_neighbors.append((current_node, neighbor))
+        current_node += 1
+    
+    graph.add_edges_from(node_neighbors)
+    # nx.draw_networkx(G=graph, pos=nx.circular_layout(graph), with_labels=True)
+    # nx.draw(graph, pos=nx.circular_layout(G=graph, dim=2))
+    nx.draw_circular(G=graph, with_labels=True, arrowstyle="-")
+    plt.show()
+
+    # info: nx rysuje node'y w zlej kolejnosci wiec pewnie recznie trzeba
+    # najpierw dodac node'y, a potem wszystkie edge
